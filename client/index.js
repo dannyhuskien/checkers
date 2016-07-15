6e4258288de2fca61184ad1d5f3d60398b3d0de0
Steven Snow and Syed Nabeel <pair+steven+syed@allstate.com>
2016-07-15 10:03:54 -0500

    Revert "ok"
    This reverts commit ea1e9f5317726f5dcbdc17a84161b43369e20b16.

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import App from './components/App';
 import Home from './components/Home';
 import About from './components/About';
 import Faq from './components/Faq';
-import Gameboard from './components/Gameboard';
 
 import React from 'react';
 import { render } from 'react-dom';
@@ -17,7 +16,6 @@ render(
       <IndexRoute component={Home} />
       <Route path="about" component={About} />
       <Route path="faq" component={Faq} />
-      Route path="game" component={Gameboard} />
     </Route>
   </Router>
   , document.getElementById('root'));
