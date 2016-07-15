ea1e9f5317726f5dcbdc17a84161b43369e20b16
Steven Snow and Syed Nabeel <pair+steven+syed@allstate.com>
2016-07-15 09:59:45 -0500

    ok

---
+++ client/components/Gameboard.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+
+
+export default () => <h1>Gaming</h1>;
+// class Gameboard extends React.Component {
+//
+//   constructor(props) {
+//     super(props);
+//     this.state = { shutup: 'rightnow' };
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>CHECKEEEERRRSSS</h1>
+//       </div>);
+//   } // End of Render() method
+//
+// } // End of Class
+//
+// export default Gameboard;
--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import App from './components/App';
 import Home from './components/Home';
 import About from './components/About';
 import Faq from './components/Faq';
+import Gameboard from './components/Gameboard';
 
 import React from 'react';
 import { render } from 'react-dom';
@@ -16,6 +17,7 @@ render(
       <IndexRoute component={Home} />
       <Route path="about" component={About} />
       <Route path="faq" component={Faq} />
+      Route path="game" component={Gameboard} />
     </Route>
   </Router>
   , document.getElementById('root'));
